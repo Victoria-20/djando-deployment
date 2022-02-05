372c28c3186188bc39ef9fcea083f120f78398cb
Victoria <68999142+Victoria-20@users.noreply.github.com>
2022-02-05 11:49:34 +0300

    Update urls.py

--- myfirstblog/myfirstblog/urls.py
+++ myfirstblog/myfirstblog/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
+from django.urls import path, include, re_path
+# from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myblog.urls')),
-    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name='logout', kwargs={'next_page':'/'}),
+    re_path(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+    re_path(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name='logout', kwargs={'next_page':'/'}),
 ]
